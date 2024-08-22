97a3c88aa91ab4fef419f183d3f1b8b6caaeaa5a
milux <michi.lux@gmail.com>
2024-08-22 18:26:31 +0200

    Use kotlin schema codegen plugin in Gradle by ID (#29)

--- csaf-schema/build.gradle.kts
+++ csaf-schema/build.gradle.kts
@@ -1,27 +1,10 @@
 import net.pwall.json.kotlin.codegen.gradle.JSONSchemaCodegen
-import net.pwall.json.kotlin.codegen.gradle.JSONSchemaCodegenPlugin
 import net.pwall.json.kotlin.codegen.gradle.JSONSchemaCodegenTask
-
-buildscript {
-    repositories {
-        maven { url = uri("https://jitpack.io") }
-        mavenCentral()
-    }
-    dependencies {
-        // A.t.m. we need our patched version of json-kotlin-schema-codegen here.
-        classpath("com.github.csaf-sbom:json-kotlin-schema-codegen:0.108.2")
-        classpath("net.pwall.json:json-kotlin-gradle:0.107") {
-            exclude("net.pwall.json", "json-kotlin-schema")
-            exclude("net.pwall.json", "json-kotlin-schema-codegen")
-        }
-    }
-}
 
 plugins {
     id("buildlogic.kotlin-library-conventions")
+    id("net.pwall.json.json-kotlin") version "0.108.2"
 }
-
-apply<JSONSchemaCodegenPlugin>()
 
 configure<JSONSchemaCodegen> {
     configFile.set(file("src/main/resources/codegen-config.json"))
