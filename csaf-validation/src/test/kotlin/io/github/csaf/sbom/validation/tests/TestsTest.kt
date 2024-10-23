4a6fbd2f60aed449a6c968ba0ccb28624cc96e2f
Christian Banse <christian.banse@aisec.fraunhofer.de>
2024-10-23 10:46:50 +0200

    Cleaner test execution

--- csaf-validation/src/test/kotlin/io/github/csaf/sbom/validation/tests/TestsTest.kt
+++ csaf-validation/src/test/kotlin/io/github/csaf/sbom/validation/tests/TestsTest.kt
@@ -18,6 +18,7 @@ package io.github.csaf.sbom.validation.tests
 
 import io.github.csaf.sbom.schema.generated.Csaf
 import io.github.csaf.sbom.validation.ValidationFailed
+import io.github.csaf.sbom.validation.ValidationResult
 import io.github.csaf.sbom.validation.ValidationSuccessful
 import io.github.csaf.sbom.validation.requirements.goodCsaf
 import kotlin.io.path.Path
@@ -26,59 +27,62 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlinx.serialization.json.Json
 
+/** The path to the test folder for the CSAF 2.0 tests. */
+var testFolder: String = "../csaf/csaf_2.0/test/validator/data/"
+
+/**
+ * Short utility function to construct the path to the test file based on the test file ID for
+ * mandatory tests.
+ */
+fun mandatoryTest(id: String): String {
+    return "$testFolder/mandatory/oasis_csaf_tc-csaf_2_0-2021-${id}.json"
+}
+
+/**
+ * Short utility function to construct the path to the test file based on the test file ID for
+ * optional tests.
+ */
+fun optionalTest(id: String): String {
+    return "$testFolder/optional/oasis_csaf_tc-csaf_2_0-2021-${id}.json"
+}
+
+/** Extension function to test a JSON file given in the [path]. */
+fun io.github.csaf.sbom.validation.Test.test(path: String): ValidationResult {
+    val doc = Json.decodeFromString<Csaf>(Path(path).readText())
+    return this.test(doc)
+}
+
 class TestsTest {
     @Test
     fun test611() {
         val test = Test611MissingDefinitionOfProductID
-        val fail =
-            Json.decodeFromString<Csaf>(
-                Path(
-                        "../csaf/csaf_2.0/test/validator/data/mandatory/oasis_csaf_tc-csaf_2_0-2021-6-1-01-01.json"
-                    )
-                    .readText()
-            )
 
         assertEquals(
             ValidationFailed(
                 listOf("The following IDs are not defined: CSAFPID-9080700,CSAFPID-9080701")
             ),
-            test.test(fail)
+            test.test(mandatoryTest("6-1-01-01"))
         )
     }
 
     @Test
     fun test612() {
         val test = Test612MultipleDefinitionOfProductID
-        val fail =
-            Json.decodeFromString<Csaf>(
-                Path(
-                        "../csaf/csaf_2.0/test/validator/data/mandatory/oasis_csaf_tc-csaf_2_0-2021-6-1-02-01.json"
-                    )
-                    .readText()
-            )
 
         assertEquals(
             ValidationFailed(listOf("The following IDs are duplicate: CSAFPID-9080700")),
-            test.test(fail)
+            test.test(mandatoryTest("6-1-02-01"))
         )
     }
 
     @Test
     fun test613() {
         val test = Test613CircularDefinitionOfProductID
-        val fail =
-            Json.decodeFromString<Csaf>(
-                Path(
-                        "../csaf/csaf_2.0/test/validator/data/mandatory/oasis_csaf_tc-csaf_2_0-2021-6-1-03-01.json"
-                    )
-                    .readText()
-            )
 
         assertEquals(
             ValidationFailed(listOf("The following IDs are defined in circles: CSAFPID-9080701")),
-            test.test(fail)
+            test.test(mandatoryTest("6-1-03-01"))
         )
-
         assertEquals(ValidationSuccessful, test.test(goodCsaf(productTree = null)))
         assertEquals(ValidationSuccessful, test.test(goodCsaf(productTree = Csaf.ProductTree())))
     }
@@ -86,34 +90,20 @@ class TestsTest {
     @Test
     fun test614() {
         val test = Test614MissingDefinitionOfProductGroupID
-        val fail =
-            Json.decodeFromString<Csaf>(
-                Path(
-                        "../csaf/csaf_2.0/test/validator/data/mandatory/oasis_csaf_tc-csaf_2_0-2021-6-1-04-01.json"
-                    )
-                    .readText()
-            )
 
         assertEquals(
             ValidationFailed(listOf("The following IDs are not defined: CSAFGID-1020301")),
-            test.test(fail)
+            test.test(mandatoryTest("6-1-04-01"))
         )
     }
 
     @Test
     fun test615() {
         val test = Test615MultipleDefinitionOfProductGroupID
-        val fail =
-            Json.decodeFromString<Csaf>(
-                Path(
-                        "../csaf/csaf_2.0/test/validator/data/mandatory/oasis_csaf_tc-csaf_2_0-2021-6-1-05-01.json"
-                    )
-                    .readText()
-            )
 
         assertEquals(
             ValidationFailed(listOf("The following IDs are duplicate: CSAFGID-1020300")),
-            test.test(fail)
+            test.test(mandatoryTest("6-1-05-01"))
         )
     }
 
@@ -130,7 +120,7 @@ class TestsTest {
 
         assertEquals(
             ValidationFailed(listOf("The following IDs are not used: CSAFPID-9080700")),
-            test.test(fail)
+            test.test(optionalTest("6-2-01-01"))
         )
     }
 
