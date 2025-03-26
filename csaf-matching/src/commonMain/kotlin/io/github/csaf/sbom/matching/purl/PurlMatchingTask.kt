4ed1ccdfbe68ea746a135438449cb88b09565f0d
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-03-26 10:38:37 +0100

    Restructed matching value

--- csaf-matching/src/commonMain/kotlin/io/github/csaf/sbom/matching/purl/PurlMatchingTask.kt
+++ csaf-matching/src/commonMain/kotlin/io/github/csaf/sbom/matching/purl/PurlMatchingTask.kt
@@ -20,35 +20,60 @@ import io.github.csaf.sbom.matching.MatchingTask
 import protobom.protobom.Node
 import protobom.protobom.SoftwareIdentifierType
 
-const val DEFINITELY_NO_MATCH = 0.0f
-const val DEFINITE_MATCH = 1.0f
-const val MATCH_PACKAGE_NO_VERSION = 0.7f
+interface MatchingConfidence {
+    val value: Float
+}
 
-fun Purl.matches(other: Purl): Float {
+/** A [DefiniteMatch] indicates a definite match. This is the highest possible match value. */
+object DefiniteMatch : MatchingConfidence {
+    override val value = 1.0f
+}
+
+/** A [DefinitelyNoMatch] indicates a definite no match. This is the lowest possible match value. */
+object DefinitelyNoMatch : MatchingConfidence {
+    override val value = 0.0f
+}
+
+/**
+ * A [MatchPackageNoVersion] indicates a match, but the version is not set. This is a partial match
+ * because we consider that semantically means that the package is affected, but we do not know
+ * which version. So in theory, all versions that are in the SBOM could be a match. It is not a
+ * definite match, but it is also not a no match. It is a partial match.
+ */
+object MatchPackageNoVersion : MatchingConfidence {
+    override val value = 0.7f
+}
+
+/** A [MatcherNotSuitable] indicates that the matcher is not suitable for the given component. */
+object MatcherNotSuitable : MatchingConfidence {
+    override val value = -1.0f
+}
+
+fun Purl.matches(other: Purl): MatchingConfidence {
     // All of these must match, otherwise we definitely not have a match. We can skip comparing the
     // scheme because the schema is already checked in the Purl constructor.
-    if (this.getType() != other.getType()) return DEFINITELY_NO_MATCH
-    if (this.getNamespace() != other.getNamespace()) return DEFINITELY_NO_MATCH
-    if (this.getName() != other.getName()) return DEFINITELY_NO_MATCH
+    if (this.getType() != other.getType()) return DefinitelyNoMatch
+    if (this.getNamespace() != other.getNamespace()) return DefinitelyNoMatch
+    if (this.getName() != other.getName()) return DefinitelyNoMatch
 
     // If the version is not set, we have a match, but we are not completely sure. It could either
     // mean that someone forgot to set the version or that that all versions are affected.
-    if (this.getVersion() == null) return MATCH_PACKAGE_NO_VERSION
+    if (this.getVersion() == null) return MatchPackageNoVersion
 
     // If the version is set, we have a match if the versions are equal
-    if (this.getVersion() == other.getVersion()) return DEFINITE_MATCH
+    if (this.getVersion() == other.getVersion()) return DefiniteMatch
 
-    return DEFINITELY_NO_MATCH
+    return DefinitelyNoMatch
 }
 
 class PurlMatchingTask(val purl: Purl) : MatchingTask {
 
-    override fun match(component: Node): Float {
+    override fun match(component: Node): MatchingConfidence {
         // Check, if we have a purl to match against
         val componentPurl =
             component.identifiers[SoftwareIdentifierType.PURL.value]?.let { Purl(it) }
         if (componentPurl == null) {
-            return DEFINITELY_NO_MATCH
+            return MatcherNotSuitable
         }
 
         return purl.matches(componentPurl)
